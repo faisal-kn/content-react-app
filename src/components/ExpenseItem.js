959649a3dacdee0cf51033e569d43af3c83cf9e0
faisal-kn <khanmohdfaisal75@gmail.com>
2021-07-20 16:42:56 +0530

    Added css to expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,6 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  console.log(props);
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
