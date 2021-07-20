bb83d1fe52f760960997c1e6e374fd0378af55ef
faisal-kn <khanmohdfaisal75@gmail.com>
2021-07-20 14:28:43 +0530

    created expense date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   console.log(props);
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleString()}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <h2 className="expense-item__description">{props.title}</h2>
       <div className="expense-item__price">{props.amount}</div>
     </div>
